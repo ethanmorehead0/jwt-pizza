7e71eb9193265f168e8be6da7d83a3c5dfb062a3
ethan <ethan0@byu.edu>
2025-02-13 23:09:03 -0700

    deleted tester files

--- tests/login.spec.ts
+++
@@ -1,9 +0,0 @@
-import {test, expect} from 'playwright-test-coverage';
-
-test('home page', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
-    await expect(page).toHaveTitle(/Playwright/);
-
-
-
-  });
